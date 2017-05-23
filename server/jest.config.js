ea961fea925e593faca034273a4720eb2f8abe89
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2017-05-23 11:53:36 +0200

    Tests: Run migration codemods on server tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+	modulePaths: [
+		'<rootDir>/../test/',
+		'<rootDir>/../server/',
+		'<rootDir>/../client/',
+		'<rootDir>/../client/extensions/',
+	],
+	testEnvironment: 'node',
 	testMatch: [ '**/test/*.js?(x)' ],
 	timers: 'fake',
 	verbose: true,
