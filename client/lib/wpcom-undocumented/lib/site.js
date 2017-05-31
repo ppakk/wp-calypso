646a87c2f43980d7c1f3905eeb1ce40a67ecd067
John Godley <johng75@gmail.com>
2017-05-31 09:53:33 +0100

    API class for media service

--- client/lib/wpcom-undocumented/lib/site.js
+++ client/lib/wpcom-undocumented/lib/site.js
@@ -256,6 +256,16 @@ UndocumentedSite.prototype.getConnection = function( connectionId ) {
 	} );
 };
 
+UndocumentedSite.prototype.uploadExternalMedia = function( serviceName, files ) {
+	debug( '/sites/:site_id:/publicize-connections/:connection_id: query' );
+
+	return this.wpcom.req.post( {
+		path: '/sites/' + this._id + '/external-media-upload',
+	}, {
+		external_ids: files, service: serviceName
+	} );
+};
+
 /**
  * Runs Theme Setup (Headstart).
  *
