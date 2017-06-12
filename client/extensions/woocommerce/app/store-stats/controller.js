fd2bf1741ecb5d5abcb1003e00cff23b6b3c8248
Paul Sealock <psealock@gmail.com>
2017-06-13 10:29:48 +1200

    Fix queryDate for previous periods

--- client/extensions/woocommerce/app/store-stats/controller.js
+++ client/extensions/woocommerce/app/store-stats/controller.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import page from 'page';
 import includes from 'lodash/includes';
+import { moment } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -11,6 +12,7 @@ import includes from 'lodash/includes';
 import { renderWithReduxStore } from 'lib/react-helpers';
 import AsyncLoad from 'components/async-load';
 import StatsPagePlaceholder from 'my-sites/stats/stats-page-placeholder';
+import { UNITS } from './constants';
 
 function isValidParameters( context ) {
 	const validParameters = {
@@ -21,16 +23,28 @@ function isValidParameters( context ) {
 		.every( param => includes( validParameters[ param ], context.params[ param ] ) );
 }
 
+function getQueryDate( context ) {
+	const { unit } = context.params;
+	const unitConfig = UNITS[ unit ];
+	const today = moment();
+	const startDate = moment( context.query.startDate ); // Defaults to today if startDate undefined
+	const unitQuantity = unitConfig.quantity;
+	const duration = Math.floor( moment.duration( today - startDate )[ unitConfig.durationFn ]() );
+	const periods = Math.floor( duration / unitQuantity ) * unitQuantity;
+	return today.subtract( periods, unitConfig.label ).format( 'YYYY-MM-DD' );
+}
+
 export default function StatsController( context ) {
 	if ( ! isValidParameters( context ) ) {
 		page.redirect( `/store/stats/orders/day/${ context.params.site }` );
 	}
-
+	const queryDate = getQueryDate( context );
 	const props = {
 		type: context.params.type,
 		unit: context.params.unit,
 		path: context.pathname,
-		startDate: context.query.startDate,
+		queryDate,
+		selectedDate: context.query.startDate || moment().format( 'YYYY-MM-DD' ),
 	};
 	renderWithReduxStore(
 		<AsyncLoad
