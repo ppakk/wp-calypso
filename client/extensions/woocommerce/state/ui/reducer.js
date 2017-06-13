a1f51d2a8e0bcfcad7a53e0ba6d7c5020c9d73c4
Kelly Dwan <ryelle@users.noreply.github.com>
2017-06-13 15:46:59 +0200

    Add a reducer/action to track the current page of the orders list

--- client/extensions/woocommerce/state/ui/reducer.js
+++ client/extensions/woocommerce/state/ui/reducer.js
@@ -1,11 +1,13 @@
 /**
  * Internal dependencies
  */
+import orders from './orders/reducer';
 import products from './products/reducer';
 import shipping from './shipping/reducer';
 import { combineReducers } from 'state/utils';
 
 export default combineReducers( {
+	orders,
 	products,
 	shipping,
 } );
