123b6b839ec51e3c84dd91cc6cc694b150c9964e
Jeff Bowen <jeff@automattic.com>
2017-06-13 12:34:44 -0400

    Support non-en locales

--- client/login/index.js
+++ client/login/index.js
@@ -11,8 +11,8 @@ import { makeLayout, redirectLoggedIn, setUpLocale } from 'controller';
 
 export default router => {
 	if ( config.isEnabled( 'login/magic-login' ) ) {
-		router( '/log-in/link', redirectLoggedIn, magicLogin, makeLayout );
-		router( '/log-in/link/use', redirectLoggedIn, magicLoginUse, makeLayout );
+		router( '/log-in/link/:lang?', setUpLocale, redirectLoggedIn, magicLogin, makeLayout );
+		router( '/log-in/link/use/:lang?', setUpLocale, redirectLoggedIn, magicLoginUse, makeLayout );
 	}
 
 	if ( config.isEnabled( 'login/wp-login' ) ) {
